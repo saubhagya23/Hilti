7dffb035cddb869f80f9b94d73bbb4ec8253f65a
puja goyal <puja.goyal@tothenew.com>
2017-11-24 09:46:03 +0530

    fixed bug

--- src/views/screens/travelOverview.js
+++ src/views/screens/travelOverview.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image,TouchableOpacity } from 'react-native';
 import { FileSystem } from 'expo';
 import PageHeader from '../common/pageHeader'
 
