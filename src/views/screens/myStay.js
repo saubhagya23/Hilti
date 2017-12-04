6a961389e1724cdf4329f1576acc65ff70de9009
puja goyal <puja.goyal@tothenew.com>
2017-12-04 17:02:02 +0530

    rectifies error

--- src/views/screens/myStay.js
+++ src/views/screens/myStay.js
@@ -81,7 +81,7 @@ class MyStay extends Component {
                                             <View style={{flexDirection:'row',flexWrap:'wrap'}}>
                                                 {
                                                     details.Inclusions.map((data,index)=>(
-                                                        <View style={{width:100, height:40, marginTop:5g,marginLeft:50.5,flexDirection:'row'}} key={index}>
+                                                        <View style={{width:100, height:40, marginTop:10,marginLeft:50.5,flexDirection:'row'}} key={index}>
                                                             <Icon
                                                              name="circle"
                                                              style={{color:'#dd2127'}}
@@ -103,7 +103,7 @@ class MyStay extends Component {
                                             <View style={{flexDirection:'row',flexWrap:'wrap',marginBottom:15}}>
                                                 {
                                                     details.ImportantToRemember.map((data,index)=>(
-                                                        <View style={{width:100, height:40, marginTop:5,marginLeft:50.5,flexDirection:'row'}} key={index}>
+                                                        <View style={{width:100, height:40, marginTop:10,marginLeft:50.5,flexDirection:'row'}} key={index}>
                                                             <Icon
                                                                 name="circle"
                                                                 style={{color:'#dd2127'}}
