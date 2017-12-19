47c264f101ec78a22d724a311b0aca6d743f7653
saubhagya23 <saubhagya.nair@tothenew.com>
2017-12-19 16:52:09 +0530

    Marker added to venue.

--- src/views/screens/venue.js
+++ src/views/screens/venue.js
@@ -37,7 +37,7 @@ class Venue extends Component {
     }
 
     findViaGMap = () => {
-        var url = 'geo:' + '28.6621,77.3021';
+        var url = 'geo:' + '28.6621,77.3021'+'?q=Leela Ambience Convention Hotel,label=L';
         this.openExternalApp(url);
     }
 
